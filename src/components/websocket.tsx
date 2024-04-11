98462e6a124916c35df4e0682151bf2e9de983d2
MoritzMix <moritz.mix@web.de>
2024-04-11 10:01:05 +0200

    ws message updates data

--- src/components/websocket.tsx
+++ src/components/websocket.tsx
@@ -1,14 +1,17 @@
 "use client";
 import React, { useEffect, useState } from "react";
-
+import { useRouter } from "next/navigation";
 import io from "socket.io-client";
 const socket = io("http://localhost:3000");
 
 export default function Websuck() {
+  const router = useRouter();
+
   useEffect(() => {
     socket.on("message2", (data) => {
       console.log("Recieved from SERVER ::", data);
       // Execute any command
+      router.refresh();
     });
   }, [socket]);
   return <div>Hello World!</div>;
