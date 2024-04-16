2ceecec59c50c46fd46facc60b4377870a58e10d
MoritzMix <moritz.mix@web.de>
2024-04-16 13:13:52 +0200

    clean

--- src/components/wsSubscribe.tsx
+++ src/components/wsSubscribe.tsx
@@ -1,19 +1,26 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import io from "socket.io-client";
 const socket = io("http://localhost:3000");
 
 export default function WsSubscribe() {
   const router = useRouter();
+
   useEffect(() => {
-    socket.on("message2", (data) => {
-      console.log("Recieved from SERVER ::", data);
+    const handleSocketMessage = (data) => {
+      console.log("Received from SERVER ::", data);
       // Execute any command
       router.refresh();
-    });
-  }, [socket]);
+    };
 
-  return <div></div>;
+    socket.on("message2", handleSocketMessage);
+
+    return () => {
+      socket.off("message2", handleSocketMessage); // Clean up the subscription
+    };
+  }, [socket, router]);
+
+  return null;
 }
